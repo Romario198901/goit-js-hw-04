e42422867db422806054d8af3efc06374f2b5b28
Roman Hrydin <romario198901@gmail.com>
2025-10-06 15:41:48 +0300

    complete tasks 2,3 in the homework

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,30 @@
 function isEnoughCapacity(products, containerSize) {
-    const productsQuantity = Object.values(products);
+  const productsQuantity = Object.values(products);
   let sum = 0;
   for (const value of productsQuantity) {
-    sum+= value;
+    sum += value;
   }
-   if(sum <= containerSize) {
-        return true;
-    }
+  if (sum <= containerSize) {
+    return true;
+  }
   return false;
-} 
-console.log (isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+}
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
 console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
 console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
 
 function isEnoughCapacity2(products, containerSize) {
-    let sum = 0;
-    for (const key in products) {
-       sum += products[key];  
-    }
-    if (sum <= containerSize) {
-        return true;
-    }
-    return false;
+  let sum = 0;
+  for (const key in products) {
+    sum += products[key];
+  }
+  if (sum <= containerSize) {
+    return true;
+  }
+  return false;
 }
-console.log (isEnoughCapacity2({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity2({ apples: 2, grapes: 3, carrots: 1 }, 8));
 console.log(isEnoughCapacity2({ apples: 4, grapes: 6, lime: 16 }, 12));
 console.log(isEnoughCapacity2({ apples: 1, lime: 5, tomatoes: 3 }, 14));
 console.log(isEnoughCapacity2({ apples: 18, potatoes: 5, oranges: 2 }, 7));
